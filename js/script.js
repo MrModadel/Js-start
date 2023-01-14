2ba0123c4ecac2221d3eb70ab7cd188f5b82e1fe
MrModadel <timanormu@gmail.com>
2023-01-14 17:57:27 +0500

    first commit

---
+++ js/script.js
@@ -0,0 +1,20 @@
+
+let admin;
+let name;
+name = "Джон";
+admin = name;
+console.log(admin);
+/* баланс */
+let balance = 25000;
+/* расходы */
+let car = 4480;
+let food = 890;
+/* даходы */
+let invoice = 5500;
+let stock = 4200;
+/* вычесление */
+let expenses_bal = balance - (car + food) ;
+let profits = expenses_bal + invoice + stock;
+/* итог */
+let totalBalance = profits;
+console.log(totalBalance);
